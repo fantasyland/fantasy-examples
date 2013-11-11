e199d5089b726eef630a4ade68db4347eefa875d
Simon Richardson <simon@ustwo.co.uk>
2013-11-11 16:07:14 +0000

    Adding fantasy-combinators - Removing the duplication of combinators lib.

--- StateIO.js
+++ StateIO.js
@@ -1,6 +1,10 @@
 var StateT = require('fantasy-states').StateT,
     IO = require('fantasy-io'),
+    combinators = require('fantasy-combinators'),
     fs = require('fs'),
+
+    compose = combinators.compose,
+    constant = combinators.constant,
 
     // Monad which contains state and can do IO.
     M = StateT(IO),
@@ -8,23 +12,11 @@ var StateT = require('fantasy-states').StateT,
     // Load 2 files, concatenate them to the start state and print the result.
     program =
         M.lift(readFile('package.json'))
-        .chain(compose(M.modify, prependTo))
+        .chain(compose(M.modify)(prependTo))
         .chain(constant(M.lift(readFile('StateIO.js'))))
-        .chain(compose(M.modify, prependTo))
+        .chain(compose(M.modify)(prependTo))
         .chain(constant(M.get))
-        .chain(compose(M.lift, println));
-
-function constant(a) {
-    return function(b) {
-        return a;
-    };
-}
-
-function compose(f, g) {
-    return function(x) {
-        return f(g(x));
-    };
-}
+        .chain(compose(M.lift)(println));
 
 function prependTo(a) {
     return function(b) {
